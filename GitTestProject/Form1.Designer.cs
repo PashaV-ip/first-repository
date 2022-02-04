839b8bf77cbfa36cc48d43cbde1e155831f78a43
Vanshev <pav-alva@ya.ru>
2022-02-04 17:48:55 +0300

    Добавлен: Запрос на выход и изменён цвет

--- GitTestProject/Form1.Designer.cs
+++ GitTestProject/Form1.Designer.cs
@@ -65,6 +65,7 @@ namespace GitTestProject
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.GreenYellow;
             this.ClientSize = new System.Drawing.Size(700, 260);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button2);
